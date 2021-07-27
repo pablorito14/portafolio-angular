3d9175555dd3777941364327790d995669ad8263
Pablo Rito <pablorito14@gmail.com>
2021-07-27 20:48:56 -0300

    fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,21 +8,54 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando:boolean = true;
   productos:any = [];
+  productosFiltrado:Producto[] = [];
 
   constructor( private http:HttpClient ) {
     this.cargarProductos();  
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-7f46c-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto) => {
-        
-        console.log(resp);
-        this.productos = resp;
-        this.cargando = false;
-        
-      } );
+    return new Promise<void>( (resolve,reject) => {
+      this.http.get('https://angular-html-7f46c-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        } );
+    } );
+    
+
+    
+  }
+  getProducto(id:string){
+    // https://angular-html-7f46c-default-rtdb.firebaseio.com/productos/prod-14.json
+    return this.http.get(`https://angular-html-7f46c-default-rtdb.firebaseio.com/productos/${id}.json`);
   }
 
+  buscarProducto(termino:string){
+    if(this.productos.length === 0){
+      this.cargarProductos().then( () => {
+        // se ejecuta despues de terminar de cargar
+        this.filtrarProductos(termino);
+      })
+    } else {
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino:string){
+    
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach((prod: any) => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
+    });
+  }
   
 }
